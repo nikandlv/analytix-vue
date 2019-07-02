827dd7b66c1195744e1cb0e46438061eca9858b3
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-02 16:57:22 +0430

    Added the proper design font

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
+  <div class="root">
     <nuxt />
   </div>
 </template>
 <style lang="stylus">
 @import '../node_modules/normalize-stylus/normalize.styl'
-
+@import url('https://fonts.googleapis.com/css?family=Muli&display=swap');
+.root
+  font-family 'Muli', sans-serif
 </style>
