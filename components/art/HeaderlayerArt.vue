4fb9e9e6f072e8533dc26dc038deac00fc170af7
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-02 17:17:20 +0430

    Added header art

---
+++ components/art/HeaderlayerArt.vue
@@ -0,0 +1,31 @@
+<template>
+    <svg class="header-layer-art" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1920" height="1540" viewBox="0 0 1920 1540">
+  <defs>
+    <clipPath id="clip-path">
+      <rect id="Rectangle_566" data-name="Rectangle 566" width="1920" height="1540" fill="#fff" stroke="#707070" stroke-width="1"/>
+    </clipPath>
+    <clipPath id="clip-path-2">
+      <rect width="2992.864" height="2983.134" fill="none"/>
+    </clipPath>
+  </defs>
+  <g id="Mask_Group_4" data-name="Mask Group 4" clip-path="url(#clip-path)">
+    <g id="Repeat_Grid_1" data-name="Repeat Grid 1" transform="translate(-279.167 -1638.841)" clip-path="url(#clip-path-2)">
+      <g id="Group_258" data-name="Group 258" transform="translate(279.167 1638.841)">
+        <path id="Path_374" data-name="Path 374" d="M-14.366,384.5,2015.832,0l5.965,1703.09L-11.546,1687.629Z" transform="matrix(0.966, -0.259, 0.259, 0.966, -141.638, -368.113)" fill="#fff"/>
+        <path id="Path_376" data-name="Path 376" d="M241.628,192.5,2424.4,0l-77.518,1984.812s-1593.5,425.909-2119.819-22.169S241.628,192.5,241.628,192.5Z" transform="matrix(0.966, -0.259, 0.259, 0.966, -276.139, -750.359)" fill="#d9e8ff"/>
+        <path id="Path_382" data-name="Path 382" d="M241.628,192.5,2424.4,0l-77.518,1984.812s-1593.5,425.909-2119.819-22.169S241.628,192.5,241.628,192.5Z" transform="matrix(0.966, -0.259, 0.259, 0.966, -189.139, -1011.359)" fill="#1771ff"/>
+      </g>
+    </g>
+  </g>
+</svg>
+</template>
+<style lang="stylus">
+.header-layer-art
+    position absolute
+    top 0
+    left 0
+    max-width 100%
+    width 100%
+    height auto
+</style>
+
--- layouts/HeaderSection.vue
+++ layouts/HeaderSection.vue
@@ -1,7 +1,20 @@
 <template>
     <header>
+        <headerlayer-art />
         <div class="top-bar">
             
         </div>
     </header>
 </template>
+<script>
+import HeaderlayerArt from '../components/art/HeaderlayerArt'
+export default {
+    components:{
+        HeaderlayerArt
+    }
+}
+</script>
+<style lang="stylus">
+header
+    position relative
+</style>
