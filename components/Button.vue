ca13ced8410a85c35abc78c39bb85024a7f4610f
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-02 23:46:37 +0430

    Added button component and top-bar

---
+++ components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+    <button>
+        <slot>Button</slot>
+    </button>
+</template>
+<style lang="stylus">
+button
+    background-color rgba(white,0.5)
+    color white
+    -webkit-box-shadow: 0px 13px 26px -16px rgba(0,0,0,0.41);
+    -moz-box-shadow: 0px 13px 26px -16px rgba(0,0,0,0.41);
+    box-shadow: 0px 13px 26px -16px rgba(0,0,0,0.41);
+    outline none 
+    border unset
+    padding 0.6rem 1.5rem
+    font-size 0.8rem
+    border-radius 1000px
+    cursor pointer
+    transition box-shadow 400ms, background-color 200ms
+    &:hover
+        box-shadow 0px 4px 5px 0px rgba(0,0,0,0.0.4), 0px 4px 2px 0px rgba(0,0,0,0.06), 0px 3px 1px -2px rgba(0,0,0,0.06)
+        background-color rgba(white,0.6)
+</style>
--- layouts/HeaderSection.vue
+++ layouts/HeaderSection.vue
@@ -2,19 +2,33 @@
     <header>
         <headerlayer-art />
         <div class="top-bar">
-            
+            <logo />
+            <div class="spacing"/>
+            <button>Free download</button>
         </div>
     </header>
 </template>
 <script>
 import HeaderlayerArt from '../components/art/HeaderlayerArt'
+import Logo from '../components/Logo'
+import Button from '../components/Button'
 export default {
     components:{
-        HeaderlayerArt
+        HeaderlayerArt,
+        Logo,
+        Button
     }
 }
 </script>
 <style lang="stylus">
 header
     position relative
+    .top-bar
+        padding 1rem 8rem
+        display flex
+        align-items center     
+        .spacing
+            flex-grow 1       
+        button
+            position relative
 </style>
