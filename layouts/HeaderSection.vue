ea73f2fdc1716c3dd97197937c3186a1173b4d2c
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-03 00:00:00 +0430

    Added header text

--- layouts/HeaderSection.vue
+++ layouts/HeaderSection.vue
@@ -6,6 +6,11 @@
             <div class="spacing"/>
             <button>Free download</button>
         </div>
+        <div class="text-section">
+            <h1>Product Analytics</h1>
+        <h1>explained</h1>
+        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
+        </div>
     </header>
 </template>
 <script>
@@ -23,6 +28,23 @@ export default {
 <style lang="stylus">
 header
     position relative
+    .text-section
+        margin-top 100px
+        h1
+            position relative
+            text-align center
+            color white
+            font-weight bold
+            font-size 3rem
+            margin 0
+        p
+            max-width 40%
+            position relative
+            display block
+            margin 36px auto
+            color rgba(white,0.7)
+            z-index 1
+            text-align center
     .top-bar
         padding 1rem 8rem
         display flex
